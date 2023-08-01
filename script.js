65d6e5073d385d7559b0ae7ffd1618fbd0d7047a
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-08-01 11:11:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+let form1 = document.getElementById("form1");
+	let fname = form1[0].value;
+	let lname = form1[1].value;
+    alert( fname  +" "+ lname);
 }
